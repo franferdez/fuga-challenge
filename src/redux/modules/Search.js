eddb3d879cf5bba728435facf9dd2ac9b5f4fb9d
Francisco Fernandez Novella <franferdez@gmail.com>
2016-04-02 20:59:35 +0200

    add Search  redux module

---
+++ src/redux/modules/Search.js
@@ -0,0 +1,75 @@
+/* @flow */
+import jsonSearch from 'static/audiosearch_example_response_body.json'
+
+// ------------------------------------
+// Constants
+// ------------------------------------
+export const SEND_SEARCH = 'SEND_SEARCH'
+export const RECEIVE_SEARCH = 'RECEIVE_SEARCH'
+
+// ------------------------------------
+// Actions
+// ------------------------------------
+export function sendSearch (): Action {
+  return {
+    type: SEND_SEARCH,
+    payload: {searching: true}
+  }
+}
+
+export function receiveSearch (response: Object = {}): Action {
+  return {
+    type: RECEIVE_SEARCH,
+    payload: {
+      searching: false,
+      response: response
+    }
+
+  }
+}
+
+export const doSearch = (query: String = ''): Function => {
+  return (dispatch: Function, getState: Function): Promise => {
+    return new Promise((resolve: Function): void => {
+      dispatch(sendSearch())
+      // In reality here is where you make the ajax call to the API
+      // In this case i return the mocked data in async
+      setTimeout(() => {
+        dispatch(receiveSearch(jsonSearch))
+        resolve()
+      }, 500)
+    })
+  }
+}
+
+// ------------------------------------
+// Reducer
+// ------------------------------------
+export const initialState = {
+  searching: false,
+  query: '',
+  totalResults: 0,
+  page: 0,
+  resultsPerPage: 0,
+  results: []
+}
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SEND_SEARCH:
+      return Object.assign(state,{
+        searching: action.payload.searching
+      });
+    case RECEIVE_SEARCH:
+      const response = action.payload.response;
+      return Object.assign(state,{
+        searching: action.payload.searching,
+        query: response.query,
+        totalResults: response.total_results,
+        page: response.page,
+        resultsPerPage: response.results_per_page,
+        results: response.results
+      });
+    default:
+      return state
+  }
+}
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
 import counter from './modules/counter'
+import Search from './modules/Search'
 
 export default combineReducers({
   counter,
+  Search,
   router
 })
