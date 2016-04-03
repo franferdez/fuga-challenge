e282392a86a3dbb46cbd8a7bd80caeb4a12b6afd
Francisco Novella <franferdez@gmail.com>
2016-04-03 21:02:55 +0200

    Display podcast properties

--- src/components/PodcastResult/PodcastResult.js
+++ src/components/PodcastResult/PodcastResult.js
@@ -1,25 +1,38 @@
 import React from 'react'
 
 type Props = {
-
+  result: Object.isRequired
 };
 export class PodcastResult extends React.Component {
   props: Props;
 
+  showCategories(categories){
+    categories.map((category)=>{
+      category.name
+    })
+  }
+
   render () {
-    console.log('results: ',this.props.results);
-    const {title} =this.props.results
+    const {
+        title,
+        description,
+        categories,
+        show_title,
+        date_created,
+        date_added
+        } = this.props.result
+
     return (
       <div className="PodcastResult">
         <h3>{title}</h3>
         <p>
-          category
+          <span>{this.showCategories(categories)}</span>
+          <span>{show_title}</span>
+          <span>{date_added}</span>
         </p>
-        <p className="PodcastResult"> "Why do cat eyes look the way they do? Can cats really see in the dark?
-          And what are they trying to tell us with that purr (you know the one)? \n\n
-          We've got the answers -- cat behavior expert Mikel Delgado helps us decode cat quirks
-          and producer Sanden Totten teaches us what's behind cats' glowing eyes. Plus: We learn about
-          other cool powers that animal eyes have, that ours don't.",
+        <p>
+          <span>{date_created}</span>
+          {description}
         </p>
       </div>
     )
--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -9,14 +9,16 @@ export class ResultList extends React.Component {
 
   render () {
     const results = this.props.results;
+    let i = 0;
     return (
       <div className="ResultList">
         <ul>
           {results.map(result => {
+            i++;
             const newChildren = React.Children.map(this.props.children, function(child) {
               return React.cloneElement(child, { result: result })
             });
-            return <li>{newChildren}</li>
+            return <li key={'result-item-'+ i}>{newChildren}</li>
           })}
         </ul>
       </div>
